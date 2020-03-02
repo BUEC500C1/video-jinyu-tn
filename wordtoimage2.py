705185b884097ae3888e78d43fdc28b6f6f038c8
jinyu-tn <55299831+jinyu-tn@users.noreply.github.com>
2020-03-02 18:14:42 -0500

    Update wordtoimage2.py

--- wordtoimage2.py
+++ wordtoimage2.py
@@ -6,10 +6,10 @@ Created on Tue Feb 18 17:37:32 2020
 @author: taimaame
 """
 
-#from PIL import Image  
-#from PIL import ImageDraw  
-#import getwordtwitter2
-#from PIL import ImageFont  
+# from PIL import Image  
+# from PIL import ImageDraw  
+# import getwordtwitter2
+# from PIL import ImageFont  
 
 def draw_test(username,keyword):  
     
@@ -22,15 +22,15 @@ def draw_test(username,keyword):
     
     #build a white background 
     #image.show()  
-    for i in range(0,len(list_word)):
-        font = ImageFont.truetype('./ComingSoon.ttf', 15)
-        word = list_word[i].encode('ascii', 'ignore').decode('ascii')
-        #word = list_word[i]
-        image = Image.new('RGB', (800, 500), color = (255, 192, 203))  
-        draw = ImageDraw.Draw(image)  
-        draw.text((5, 200), word, fill = (0,0,0), font=font)  
-        j = str(i+1)
-        image.save('../'+username+'/'+username+'_images/'+j+'.jpg',dpi=(300.0,300.0))
+#     for i in range(0,len(list_word)):
+#         font = ImageFont.truetype('./ComingSoon.ttf', 15)
+#         word = list_word[i].encode('ascii', 'ignore').decode('ascii')
+#         #word = list_word[i]
+#         image = Image.new('RGB', (800, 500), color = (255, 192, 203))  
+#         draw = ImageDraw.Draw(image)  
+#         draw.text((5, 200), word, fill = (0,0,0), font=font)  
+#         j = str(i+1)
+#         image.save('../'+username+'/'+username+'_images/'+j+'.jpg',dpi=(300.0,300.0))
 
     
     return  address
